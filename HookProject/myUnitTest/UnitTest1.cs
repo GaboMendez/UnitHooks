41bf55e3a69051bfd68f130c511547c379308c24
Gabriel <1071470@est.intec.edu.do>
2019-09-09 20:24:21 -0400

    initial commit

---
+++ HookProject/myUnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1;
+            int b = 1;
+
+            int result = 2;
+
+            int expected = a + b;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
